5acd8a88653d5cb895eac1e51022a6718da55dbf
seabar24 <sean.barrick@mymail.champlain.edu>
2024-02-11 21:54:17 -0500

    Assignment 3.1

---
+++ dns-resolver.ps1
@@ -0,0 +1,15 @@
+﻿# Resolves DNS for network prefixes
+param($network,$server)
+
+for ($prefix=1;$prefix -lt 255;$prefix++){
+
+    $ip="$network.$prefix"
+
+    $DNS=Resolve-DnsName -DnsOnly $ip -Server $server -ErrorAction Ignore | Select-Object -ExpandProperty Namehost
+    
+    if ($DNS -ne $null){
+
+        echo "$ip $DNS"
+
+    }
+}
